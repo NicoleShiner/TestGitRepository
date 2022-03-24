b994f7f5a9f1ac8e61eb3f013dc8e7beef6c38c5
rodeztazo <rodeztazo@141.19.152.105>
2022-03-24 18:55:44 +0100

    First Commit

--- src/Test01.java
+++ src/Test01.java
@@ -6,6 +6,8 @@ public class Test01 {
 
 		System.out.println("Hello World");
 		System.out.println("hello person");
+		System.out.println("Meine Veränderung");
+		System.out.println("hello");
 	}
 
 }
